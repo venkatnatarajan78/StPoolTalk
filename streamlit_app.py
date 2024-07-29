e4b2c1bd1f5c2226f5683f9ffd8a2cf6ee77de23
venkatnatarajan78 <venkat78@gmail.com>
2024-07-29 13:49:58 +0000

    added search and column selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import duckdb
 from pygwalker.api.streamlit import StreamlitRenderer
+ 
 
 # Adjust the width of the Streamlit page
 st.set_page_config(
@@ -14,16 +15,13 @@ st.write(
     "Loans securitized this month!"
 )
 
-
-#conn = sqlite3.connect('mydb.db')
-#conn=duckdb.connect('mydatabase.db')
 conn=duckdb.connect(':memory:')
 query = "SELECT * FROM read_csv('data/illd.txt')"
 df = pd.read_sql_query(query, conn)
 
 def get_pyg_renderer() -> "StreamlitRenderer":
     # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
-    return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw",kernel_computation=True)
+    return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw", kernel_computation=True)
 
 
 # Three columns with different widths
@@ -31,17 +29,39 @@ col1, col2, col3 = st.columns([3,1,1])
 # col1 is wider
 
 # Using 'with' notation:
+
+# Three columns with different widths
+col1, col2, col3 = st.columns([3,1,1])
+# col1 is wider
+
+# Using 'with' notation:
 with col1:
-    st.write('This is column 1')
+    ln_search = st.text_input("Search: Loan Identifier")
+    if ln_search:
+        query = "select * from read_csv('data/illd.txt') where " + "\"" + "Loan Identifier" + "\"" + "=" + "'" + ln_search +"'"
+        
+    df = pd.read_sql_query(query, conn)
+
 with col2:
-    st.write('Today')
-with col3:
-    st.write('Tomorrow')
+    column_list = st.multiselect (label='Customize columns to display', options=df.columns)
+    st.write(column_list)
+    no_of_cols = (len(column_list))
+    st.write(no_of_cols)    
+    query_string = "Select "
+    i=0
+    while i < no_of_cols:
+        if i == (no_of_cols-1):
+            query_string += "\"" + column_list[i] + "\" from read_csv('data/illd.txt')"
+        else: 
+            query_string += "\"" + column_list[i] + "\", "
+        i+=1
+    if column_list:
+        df = pd.read_sql_query(query_string, conn)
 
 # Insert containers separated into tabs:
-tab1, tab2, tab3 = st.tabs(["Details", "Analyze", "Something Else"])
+tab1, tab2, tab3 = st.tabs(["Dataset", "Analyze", "Details"])
 
-tab2.write("this is tab 2")
+tab2.write("To create a visual, drag and drop attributes in to X or Y axis.")
 
 # You can also use "with" notation:
 with tab1:
@@ -50,3 +70,8 @@ with tab1:
 with tab2:
     renderer = get_pyg_renderer()
     renderer.explorer()
+
+with tab3:
+    if ln_search:
+        df1=df.T
+        st.dataframe(df1, use_container_width=True)
