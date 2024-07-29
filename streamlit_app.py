67f5883b856fd10a5acfc89336a1e673cdb945bd
venkatnatarajan78 <venkat78@gmail.com>
2024-07-29 14:01:50 +0000

    tt

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,19 +16,13 @@ st.write(
 )
 
 conn=duckdb.connect(':memory:')
-query = "SELECT * FROM read_csv('data/illd.txt')"
+query = "SELECT * FROM read_csv('data/illd.txt') limit 1000"
 df = pd.read_sql_query(query, conn)
 
 def get_pyg_renderer() -> "StreamlitRenderer":
     # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
     return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw", kernel_computation=True)
 
-
-# Three columns with different widths
-col1, col2, col3 = st.columns([3,1,1])
-# col1 is wider
-
-# Using 'with' notation:
 
 # Three columns with different widths
 col1, col2, col3 = st.columns([3,1,1])
