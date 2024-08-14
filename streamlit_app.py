320a4ca1345fbb2af20ee73ae260c8d64e533aec
venkatnatarajan78 <venkat78@gmail.com>
2024-08-14 13:41:06 +0000

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,6 +52,10 @@ with col1:
 with col2:
     column_list = st.multiselect (label='Customize columns to display', options=df.columns, placeholder='Select columns')
     no_of_cols = (len(column_list))
+    if column_list:
+        select_string="Select "
+    else:
+        select_string = "Select * "
     column_string = ""
     i=0
     while i < no_of_cols:
@@ -61,7 +65,7 @@ with col2:
             column_string += "\"" + column_list[i] + "\", "
         i+=1
     #if column_list:
-    df = extract_data("Select ", column_string, from_string, filter_string)
+    df = extract_data(select_string, column_string, from_string, filter_string)
 
 # Insert containers separated into tabs:
 tab1, tab2, tab3 = st.tabs(["Dataset", "Collateral", "Details"])
